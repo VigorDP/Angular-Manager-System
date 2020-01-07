bbca2501f7db630dd6d7cd45c223de7253e2282f
wangxiaosong <1270497989@qq.com>
2020-01-07 11:10:56 +0800

    fix

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -51,29 +51,29 @@ const DIRECTIVES = [];
         image_title: false,
         images_upload_credentials: true,
         images_upload_url: '/hl/social/uploader/img/upload',
-        // images_upload_handler(blobInfo, success, failure) {
-        //   let xhr: XMLHttpRequest;
-        //   let formData: FormData;
-        //   xhr = new XMLHttpRequest();
-        //   xhr.withCredentials = false;
-        //   xhr.open('POST', '/hl/social/uploader/img/upload');
-        //   xhr.onload = () => {
-        //     let json;
-        //     if (xhr.status !== 200) {
-        //       failure('HTTP Error: ' + xhr.status);
-        //       return;
-        //     }
-        //     json = JSON.parse(xhr.responseText);
-        //     if (!json || typeof json.data !== 'string') {
-        //       failure('Invalid JSON: ' + xhr.responseText);
-        //       return;
-        //     }
-        //     success(json.data);
-        //   };
-        //   formData = new FormData();
-        //   formData.append('file', blobInfo.blob(), blobInfo.filename());
-        //   xhr.send(formData);
-        // },
+        images_upload_handler(blobInfo, success, failure) {
+          let xhr: XMLHttpRequest;
+          let formData: FormData;
+          xhr = new XMLHttpRequest();
+          xhr.withCredentials = false;
+          xhr.open('POST', '/hl/social/uploader/img/upload');
+          xhr.onload = () => {
+            let json;
+            if (xhr.status !== 200) {
+              failure('HTTP Error: ' + xhr.status);
+              return;
+            }
+            json = JSON.parse(xhr.responseText);
+            if (!json || typeof json.data !== 'string') {
+              failure('Invalid JSON: ' + xhr.responseText);
+              return;
+            }
+            success(json.data);
+          };
+          formData = new FormData();
+          formData.append('file', blobInfo.blob(), blobInfo.filename());
+          xhr.send(formData);
+        },
       },
     }),
     NgxAmapModule.forRoot({
