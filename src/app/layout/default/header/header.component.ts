d0ee9d901173e910858733329bc75c36d00b44b8
fengyinchao <15926339107@163.com>
2020-01-10 17:58:46 +0800

    feat

--- src/app/layout/default/header/header.component.ts
+++ src/app/layout/default/header/header.component.ts
@@ -13,6 +13,8 @@ export class HeaderComponent {
     title: '社区列表',
     placeholder: '请输入社区名称',
     api: this.api.getSocialProjectList,
+    renderItem: row => row.socialName,
+    renderTitle: row => row.socialName,
     propertyCompanyId: this.settings.user.propertyCompanyId,
   };
   constructor(public settings: SettingsService, public api: RestService) {}
